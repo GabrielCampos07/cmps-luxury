5464c978120d0ce344eab11d4640926d7a1f4990
GabrielCampos07 <siginmegain@hotmail.com>
2022-10-12 19:27:51 -0300

    Continuando a home

--- cmps-luxury/src/app/home/home.component.ts
+++ cmps-luxury/src/app/home/home.component.ts
@@ -6,25 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public notices = [
-    {
-      title: 'Produtos com 40% de desconto',
-      subtitle: 'Compre agora',
-      router: '',
-    },
-    {
-      title: 'Por tempo limitado: frete grátis em tênis',
-      subtitle: 'Compre agora',
-      router: '',
-    },
-    {
-      title: 'Torne-se membro e ganhe benefícios',
-      subtitle: 'Junte-se a nós',
-      router: '',
-    },
-  ];
-
   constructor() {}
 
   ngOnInit(): void {}
+
+  public scrollDown(): void {
+    window.scrollTo(0, 700);
+  }
 }
