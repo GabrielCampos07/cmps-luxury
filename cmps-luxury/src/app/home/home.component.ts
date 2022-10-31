6786710b1b042194d84d413be709d3f8e442eeb2
GabrielCampos07 <siginmegain@hotmail.com>
2022-10-31 18:49:00 -0300

    Termino primeira seção e começo da segunda

--- cmps-luxury/src/app/home/home.component.ts
+++ cmps-luxury/src/app/home/home.component.ts
@@ -6,7 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  public sectionImages: any = {};
+
+  constructor() {
+    this.sectionImages = [
+      {
+        title: 'Vestido Dionysus',
+        gender: 'Feminino',
+        price: 'R$ 2.700,00',
+        image:
+          'assets/img/models/section-home/currently-popular/currently-popular-1.svg',
+      },
+      {
+        title: 'Cardigan Ocasional',
+        gender: 'Feminino',
+        price: 'R$ 1.900,00',
+        image:
+          'assets/img/models/section-home/currently-popular/currently-popular-2.svg',
+      },
+      {
+        title: 'Blusa Inverno',
+        gender: 'Feminino',
+        price: 'R$ 1.200,00',
+        image:
+          'assets/img/models/section-home/currently-popular/currently-popular-3.svg',
+      },
+      {
+        title: 'Vestido Festivo',
+        gender: 'Feminino',
+        price: 'R$ 3.500,00',
+        image:
+          'assets/img/models/section-home/currently-popular/currently-popular-4.svg',
+      },
+      {
+        title: 'Sobretudo Invernal',
+        gender: 'Feminino',
+        price: 'R$ 3.500,00',
+        image:
+          'assets/img/models/section-home/currently-popular/currently-popular-5.svg',
+      },
+    ];
+  }
 
   ngOnInit(): void {}
 
